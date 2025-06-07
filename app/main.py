da2afe4234aa4b55a705c708c1ba47d8333328a1
phuchoang2603 <91061595+phuchoang2603@users.noreply.github.com>
2025-06-07 19:42:25 +0700

    format log to json, add request_id for better tracing (#7)
    * format log to json, add request_id for better tracing
    
    * bump to version 1.0.9
    
    * ci: update Helm values.yaml and Chart.yaml to ghcr.io/phuchoang2603/fsds-house-price-prediction:v1.0.9
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,40 +1,59 @@
 import os
-
+import time
 import joblib
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from schema import HouseInfo, HousePrediction
 from utils.data_processing import format_input_data
 from utils.logging import logger
-import time
+from utils.middleware import LogMiddleware
+from utils.middleware import RequestIDMiddleware
 
-# Creating FastAPI instance
 app = FastAPI()
+app.add_middleware(RequestIDMiddleware)
+app.add_middleware(LogMiddleware)
+
+# Load model
+model_path = os.environ.get("MODEL_PATH", "../models/model.pkl")
+logger.debug("Loading model", extra={"model_path": model_path})
+clf = joblib.load(model_path)
+logger.debug("Model loaded successfully")
 
 
-# Loading model with default path models/model.pkl
-clf = joblib.load(os.environ.get("MODEL_PATH", "../models/model.pkl"))
-
-
-# Creating an endpoint to receive the data
-# to make prediction on
 @app.post("/predict", response_model=HousePrediction)
-def predict(data: HouseInfo):
+def predict(data: HouseInfo, request: Request):
     start_time = time.time()
+
     try:
-        # Log input summary
         logger.info(
-            f"predict_received LotArea={data.LotArea} YearBuilt={data.YearBuilt} Zoning={data.MSZoning}"
+            f"predict_received lotarea={data.LotArea} yearbuilt={data.YearBuilt} zoning={data.MSZoning}",
+            extra={"request_id": request.state.request_id},
         )
 
         formatted_data = format_input_data(data)
-        price = clf.predict(formatted_data)[0]
-        duration = round((time.time() - start_time) * 1000, 2)  # in ms
+        logger.debug(
+            f"Formatted input: {formatted_data.values.tolist()}",
+            extra={"request_id": request.state.request_id},
+        )
 
-        # Log prediction result
-        logger.info(f"predict_success price={price:.2f} duration_ms={duration}")
+        price = clf.predict(formatted_data)[0]
+        duration = round((time.time() - start_time) * 1000, 2)
+
+        logger.info(
+            f"predict_success price={price:.2f} duration={duration}ms",
+            extra={"duration_ms": duration, "request_id": request.state.request_id},
+        )
 
         return HousePrediction(Price=price)
 
     except Exception as e:
-        logger.exception(f'predict_failed error="{str(e)}"')
+        logger.exception(
+            f"predict_failed error={str(e)}",
+            extra={"request_id": request.state.request_id},
+        )
         raise
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=30000, log_config=None)
---
+++ app/utils/middleware.py
@@ -0,0 +1,34 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
+from utils.logging import logger
+import uuid
+
+
+class LogMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        logger.info(
+            "Incoming request",
+            extra={
+                "req": {
+                    "method": request.method,
+                    "url": str(request.url),
+                    "client": request.client.host,
+                },
+                "res": {"status_code": response.status_code},
+            },
+        )
+        return response
+
+
+class RequestIDMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        # Try to use incoming header, otherwise generate one
+        request_id = request.headers.get("X-Request-ID", str(uuid.uuid4()))
+        request.state.request_id = request_id
+
+        # Propagate it back to client response header too
+        response: Response = await call_next(request)
+        response.headers["X-Request-ID"] = request_id
+        return response
