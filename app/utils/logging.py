126faa7aa077b21dbd3170e6a2046e919104bf32
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-06-07 19:39:21 +0700

    format log to json, add request_id for better tracing

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -1,32 +1,48 @@
 import logging
 import sys
+import os
+import json
+from logging import Formatter
 
 
-def _init_logger(name: str):
-    """Make our custom logger available
+class CustomJsonFormatter(Formatter):
+    def format(self, record):
+        json_record = {
+            "timestamp": self.formatTime(record, "%Y-%m-%dT%H:%M:%S.%fZ"),
+            "message": record.getMessage(),
+            "level": record.levelname,
+        }
 
-    Args:
-        name (str): Name of the logger
+        optional_fields = ["duration_ms", "request_id", "req", "res", "error"]
 
-    Returns:
-        Logger: A logger object
-    """
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    # Explicitly set the output of the logger to stdout,
-    # this is the default stream for normal program output.
-    # By default, it is redirected to terminal
+        for field in optional_fields:
+            value = record.__dict__.get(field)
+            if value is not None:
+                json_record[field] = value
+
+        if record.levelno == logging.ERROR and record.exc_info:
+            json_record["exception"] = self.formatException(record.exc_info)
+
+        return json.dumps(json_record)
+
+
+def _init_logger():
+    logger = logging.getLogger()
+    logger.propagate = False
+
+    # Set log level from env
+    log_level_str = os.getenv("LOG_LEVEL", "INFO").upper()
+    log_level = getattr(logging, log_level_str, logging.INFO)
+    logger.setLevel(log_level)
+
     handler = logging.StreamHandler(sys.stdout)
-    # Format the log message
-    formatter = logging.Formatter(
-        '%(levelname)s:     %(name)s:%(module)s - "%(message)s"',
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    handler.setFormatter(formatter)
-    # Add the handler to the logger
-    logger.addHandler(handler)
+    handler.setFormatter(CustomJsonFormatter())
+    logger.handlers = [handler]
+
+    # Disable Uvicorn access log
+    logging.getLogger("uvicorn.access").disabled = True
+
     return logger
 
 
-# Initialize the logger
-logger = _init_logger(name="hpp-logger")
+logger = _init_logger()
