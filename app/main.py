4610af9c247faa4b983da487f13fbd6da397e03a
phuchoang2603 <91061595+phuchoang2603@users.noreply.github.com>
2025-06-07 17:53:48 +0700

    better logging (#6)
    * better logging
    
    * bump version to 1.0.8
    
    * ci: update Helm values.yaml and Chart.yaml to ghcr.io/phuchoang2603/fsds-house-price-prediction:v1.0.8
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from schema import HouseInfo, HousePrediction
 from utils.data_processing import format_input_data
 from utils.logging import logger
+import time
 
 # Creating FastAPI instance
 app = FastAPI()
@@ -18,10 +19,22 @@ clf = joblib.load(os.environ.get("MODEL_PATH", "../models/model.pkl"))
 # to make prediction on
 @app.post("/predict", response_model=HousePrediction)
 def predict(data: HouseInfo):
-    # Predicting the class
-    logger.info("Make predictions...")
-    # Convert data to pandas DataFrame and make predictions
-    price = clf.predict(format_input_data(data))[0]
+    start_time = time.time()
+    try:
+        # Log input summary
+        logger.info(
+            f"predict_received LotArea={data.LotArea} YearBuilt={data.YearBuilt} Zoning={data.MSZoning}"
+        )
 
-    # Return the result
-    return HousePrediction(Price=price)
+        formatted_data = format_input_data(data)
+        price = clf.predict(formatted_data)[0]
+        duration = round((time.time() - start_time) * 1000, 2)  # in ms
+
+        # Log prediction result
+        logger.info(f"predict_success price={price:.2f} duration_ms={duration}")
+
+        return HousePrediction(Price=price)
+
+    except Exception as e:
+        logger.exception(f'predict_failed error="{str(e)}"')
+        raise
